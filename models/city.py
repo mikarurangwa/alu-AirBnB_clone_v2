050ad1c255dd58860a06c3d80054ea71179f1d7c
walo450 <hirwawalid43@gmail.com>
2024-09-25 18:26:35 +0200

    tests

--- models/city.py
+++ models/city.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""This is the city class"""
-import models
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    # initialize class for file/db storage type
+    """ The city class, contains state ID and name """
+
     __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    places = relationship("Place", backref="cities")
